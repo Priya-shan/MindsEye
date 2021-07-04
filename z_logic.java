01138b071a681aa36e29f18808bbdd3d707a9a2b
Shanmuga Priya M <67195594+Priya-shan@users.noreply.github.com>
2021-07-04 07:21:32 +0530

    Add files via upload

---
+++ z_logic.java
@@ -0,0 +1,24 @@
+/*
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
+package toycathon;
+
+
+import java.util.ArrayList;
+import javax.swing.JFrame;
+
+/**
+*
+* @author Kirthana Balasubramanian
+*/
+public class z_logic extends JFrame {
+        ArrayList<String> A = new ArrayList<>();
+        ArrayList<String> B = new ArrayList<>();
+   public static void main(String args[]){
+        System.out.println("execution started");
+          
+   }  
+}
+   
